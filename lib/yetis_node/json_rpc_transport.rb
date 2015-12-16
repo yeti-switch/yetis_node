56f25bb8f4ac843ac55609312abb5863c61a5bcd
Denis Talakevich <denis.t@didww.com>
2015-12-16 23:21:55 +0200

    do not destroy json_rpc client in JsonRpcTransport after request

--- lib/yetis_node/json_rpc_transport.rb
+++ lib/yetis_node/json_rpc_transport.rb
@@ -3,18 +3,18 @@ module YetisNode
   class JsonRpcTransport < BaseTransport
 
     def rpc_send(*args)
-      jrpc = json_rpc
       begin
-        jrpc.invoke_request(*args)
+        json_rpc.connect if json_rpc.closed?
+        result = json_rpc.invoke_request(*args)
+        json_rpc.close
+        result
       rescue ::JRPC::Error => e
         raise Error.new("JSON RPC Error: #{"(#{e.code}) " if e.respond_to?(:code)}#{e.message}")
-      ensure
-        jrpc.close
       end
     end
 
     def json_rpc
-      ::JRPC::TcpClient.new uri,
+      @json_rpc ||= ::JRPC::TcpClient.new uri,
                             namespace: 'yeti.',
                             timeout: options.fetch(:timeout, BaseTransport::DEFAULT_TIMEOUT)
     end
