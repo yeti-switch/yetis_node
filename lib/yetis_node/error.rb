8580c5b11058ed2b9b97c995bebbaa8feb240e6b
Denis Talakevich <denis.t@didww.com>
2015-12-16 22:58:25 +0200

    improve Error for correct exception wrapping

--- lib/yetis_node/error.rb
+++ lib/yetis_node/error.rb
@@ -1,4 +1,11 @@
 module YetisNode
   class Error < RuntimeError
+    attr_reader :original
+
+    def initialize(msg, original=$!)
+      super(msg)
+      @original = original
+    end
+
   end
 end
